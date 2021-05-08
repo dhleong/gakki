4e396e7a884019f925432a512db33fcb1108e7e9
Daniel Leong <me@dhleong.net>
2021-05-08 11:22:10 -0400

    Cache audio files to improve reliability
    The youtube download seemed to fail halfway through when piping it
    directly into the speaker, but it can complete if we aren't artificially
    throttling the download speed.

---
+++ src/core/gakki/player/caching.cljs
@@ -0,0 +1,66 @@
+(ns gakki.player.caching
+  (:require [applied-science.js-interop :as j]
+            ["env-paths" :as env-paths]
+            ["fs" :rename {createWriteStream create-write-stream
+                           createReadStream create-read-stream}]
+            ["fs/promises" :as fs]
+            ["path" :as path]
+            [promesa.core :as p]
+            ["stream" :as stream]))
+
+(def ^:private cache-dir
+  (-> (env-paths "gakki", #js {:suffix ""})
+      (j/get :cache)))
+
+(defn- open-stream [path]
+  (p/create
+    (fn [p-resolve p-reject]
+      (let [s (create-read-stream path)]
+        (doto s
+          (.on "error" p-reject)
+          (.on "open" #(p-resolve s)))))))
+
+(defn- complete-download [tmp-path destination-path]
+  (-> (fs/rename tmp-path destination-path)
+      (p/catch
+        (fn [err]
+          (println "Error completing download:" err)) )))
+
+(defn- caching-transform [^js stream destination-path]
+  (p/let [tmp-path (str destination-path ".progress")
+          ^js output (create-write-stream tmp-path)]
+
+    (p/create
+      (fn [p-resolve p-reject]
+        (doto stream
+          (.pipe output)
+          (.on "error" p-reject)
+          (.once "readable" p-resolve)
+          (.on "end" (fn []
+                       (println "Completed download of " destination-path)
+                       (complete-download tmp-path destination-path)))
+          (.resume))
+        (.on output "error" identity)))
+
+    (open-stream tmp-path)))
+
+(defn caching [^String cache-key, promise-factory]
+  (let [file-path (path/join cache-dir cache-key)]
+    (-> (p/let [_ (fs/mkdir cache-dir #js {:recursive true})
+                stream (open-stream file-path)]
+          (println "opened cached")
+          ; TODO extract config
+          {:stream stream
+           :config {:sample-rate 48000
+                    :channels 2
+                    :codec "opus"
+                    :container "webm"}})
+
+        (p/catch
+          (fn [_]
+            ; probably, we don't have it cached
+            ; TODO we could potentially resume a partial download?
+            (p/let [{:keys [stream config]} (promise-factory)
+                    stream (caching-transform stream file-path)]
+              {:stream stream
+               :config config}))))))
--- src/core/gakki/player/ytm.cljs
+++ src/core/gakki/player/ytm.cljs
@@ -1,9 +1,11 @@
 (ns gakki.player.ytm
   (:require [applied-science.js-interop :as j]
+            ["fs" :as fs]
             ["node-fetch" :as fetch]
             ["ytdl-core" :as ytdl]
             [promesa.core :as p]
             [gakki.util.convert :refer [->int]]
+            [gakki.player.caching :refer [caching]]
             [gakki.player.promised :refer [promise->playable]]))
 
 (defn- youtube-id->stream [id]
@@ -17,6 +19,8 @@
 
                   ; NOTE: You'd think these would be integers, but...
                   ; they might not be.
+                  :duration (->int (j/get fmt :approxDurationMs))
+                  :loudness-db (->int (j/get fmt :loudnessDb))
                   :sample-rate (->int (j/get fmt :audioSampleRate))
                   :channels (->int (j/get fmt :audioChannels))}
 
@@ -28,7 +32,9 @@
 
 (defn youtube-id->playable [id]
   (promise->playable
-    (youtube-id->stream id)))
+    (caching
+      (str "ytm." id)
+      #(youtube-id->stream id))))
 
 (comment
   (p/let [id "8FV4gcs-MNA"
@@ -36,9 +42,31 @@
           fmt (ytdl/chooseFormat
                 (j/get info :formats)
                 #js {:quality "highestaudio"})]
-    (println fmt))
+    (println (-> fmt
+                 (js->clj :keywordize-keys true)
+                 (assoc :url "<url>")
+                 (assoc :s "<s>")
+                 str)))
+
+  (p/let [{:keys [stream config]} (youtube-id->stream "8FV4gcs-MNA")]
+    (println config)
+    (-> stream
+        (.on "end" (fn []
+                     (println "done!")))
+        (.on "error" (fn [e]
+                       (println "ERR" e)))
+        (.pipe (fs/createWriteStream "/Users/daniel/Library/Caches/gakki/ycm.cache"))
+        (.on "error" (fn [e]
+                       (println "ERR" e)))
+        )
+    (.resume stream))
 
   (def playable
     (doto (youtube-id->playable "8FV4gcs-MNA")
-      (gakki.player.core/set-volume 0.5)
-      (gakki.player.core/play))))
+      (gakki.player.core/set-volume 0.25)
+      (gakki.player.core/play)))
+
+  (gakki.player.core/set-volume playable 0.05)
+  (gakki.player.core/play playable)
+  (gakki.player.core/pause playable)
+  )
