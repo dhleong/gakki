7ad07ce824046a4dbeddd300cb17753b6e79b84e
Daniel Leong <me@dhleong.net>
2021-05-28 09:51:58 -0400

    Fix hot-reload sometimes breaking reactive rendering in dev
    Fixes #1
    
    From the comments:
    
          ; NOTE: This is a brute-force way to ensure hot-reloads don't
          ; break reactive rendering (see #1). You would think we could use
          ;   (.rerender instance app)
          ; but that doesn't seem to be any different from just running
          ;   (k/render app)
          ; again. Clearing and unmounting the old instance works, however,
          ; and should not have any effect on the production app.

--- src/cli/gakki/cli.cljs
+++ src/cli/gakki/cli.cljs
@@ -10,9 +10,24 @@
             [gakki.util.logging :as logging]
             [gakki.views :as views]))
 
+(defonce ^:private ink-instance (atom nil))
+
 (defn ^:dev/after-load mount-root []
   (re-frame/clear-subscription-cache!)
-  (k/render (r/as-element [views/main])))
+
+  (let [app (r/as-element [views/main])]
+    (when-let [^js instance @ink-instance]
+      ; NOTE: This is a brute-force way to ensure hot-reloads don't
+      ; break reactive rendering (see #1). You would think we could use
+      ;   (.rerender instance app)
+      ; but that doesn't seem to be any different from just running
+      ;   (k/render app)
+      ; again. Clearing and unmounting the old instance works, however,
+      ; and should not have any effect on the production app.
+      (.clear instance)
+      (.unmount instance))
+
+    (reset! ink-instance (k/render app))))
 
 (defn ^:export init []
   (set! (.-title js/process) "gakki")
