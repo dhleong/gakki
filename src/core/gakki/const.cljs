c6dbc08d26b6c93938fc6fa7c8a69d9fea403e65
Daniel Leong <me@dhleong.net>
2021-06-01 14:55:08 -0400

    Slightly increase the amount of volume suppression

--- src/core/gakki/const.cljs
+++ src/core/gakki/const.cljs
@@ -3,7 +3,7 @@
 (def debug? goog/DEBUG)
 
 (def max-volume-int 10)
-(def suppressed-volume-percent 0.25)
+(def suppressed-volume-percent 0.20)
 
 (goog-define discord-app-id "")
 (goog-define discord-oauth-secret "")
