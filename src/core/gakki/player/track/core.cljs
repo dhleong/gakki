dhleong/gakki

commit d8fa7a62c0ca1c201ff083c873b7f673d66685db

Ensure that (IAudioTrack/play) is a nop after (close)
See #32

Haven't ben able to reproduce the bug but this seems like the most
likely candidate as the cause. My suspicion is that one of the async
processes kicked off in (play) is taking some time, and in the meantime
(close) gets called when the track is swapped. Since there were no
checks of the :closed? state when they return, it creates the clip
anyway and starts playing.

src/core/gakki/player/track/core.cljs
@@ -8,6 +8,23 @@
 
 (def ^:private log (log/of :player/track))
 
+(defn- create-clip [{:keys [clip seek-time seek-bytes] :as current-state}
+                    config stream]
+  ; Just in case, if there's any old clip lingering, close it:
+  (when clip
+    (clip/close clip))
+
+  (let [full-config (assoc config :start-time-seconds (or seek-time 0))
+        clip (clip/from-stream
+               (seek/nbytes-chunkwise stream (or seek-bytes 0))
+               full-config)]
+    (log "playing " stream " with " full-config
+         "; seek-bytes=" seek-bytes)
+    (doto clip
+      (clip/set-volume (:volume current-state 1.0))
+      (clip/play))
+    (assoc current-state :clip clip)))
+
 (defprotocol IAudioTrack
   (id [this])
   (close [this])
@@ -30,7 +47,9 @@
     (swap! state (fn [{:keys [clip] :as state}]
                    (when clip
                      (clip/close clip))
-                   (dissoc state :clip))))
+                   (-> state
+                       (dissoc :clip)
+                       (assoc :closed? true)))))
 
   (read-config [_this]
     (pcm/read-config source))
@@ -63,41 +82,29 @@
       false))
 
   (play [this]
-    (let [clip (:clip @state)]
-      (cond
-        (and clip (clip/default-output-device? clip))
-        (clip/play clip)
+    (let [{:keys [clip closed?]} @state]
+      (when-not closed?
+        (cond
+          (and clip (clip/default-output-device? clip))
+          (clip/play clip)
 
-        ; We have a clip, but it's targeting the wrong device
-        clip
-        (let [seek-time (clip/current-time this)]
-          ; Close this clip, seek to where we were, and start again
-          (close this)
-          (p/do!
-            (seek this seek-time)
-            (clip/play this)))
+          ; We have a clip, but it's targeting the wrong device
+          clip
+          (let [seek-time (clip/current-time this)]
+            ; Close this clip, seek to where we were, and start again
+            (close this)
+            (p/do!
+              (seek this seek-time)
+              (clip/play this)))
 
-        :else
-        (p/plet [stream (pcm/open-read-stream source)
-                 config (pcm/read-config source)]
-          (>evt [:player/on-playback-config-resolved id config])
-          (swap!
-            state
-            (fn create-clip [{:keys [clip seek-time seek-bytes] :as current-state}]
-              ; Just in case, if there's any old clip lingering, close it:
-              (when clip
-                (clip/close clip))
-
-              (let [full-config (assoc config :start-time-seconds (or seek-time 0))
-                    clip (clip/from-stream
-                           (seek/nbytes-chunkwise stream (or seek-bytes 0))
-                           full-config)]
-                (log "playing " stream " with " full-config
-                     "; seek-bytes=" seek-bytes)
-                (doto clip
-                  (clip/set-volume (:volume current-state 1.0))
-                  (clip/play))
-                (assoc current-state :clip clip))))))))
+          :else
+          (p/plet [stream (pcm/open-read-stream source)
+                   config (pcm/read-config source)]
+            (if (:closed? @state)
+              (log this " Resolved stream and config when already closed")
+              (do
+                (>evt [:player/on-playback-config-resolved id config])
+                (swap! state create-clip config stream))))))))
 
   (pause [this]
     (when-let [clip (:clip @state)]
@@ -112,8 +119,7 @@
           (clip/close clip)
           (swap! state dissoc :clip))
 
-        (clip/pause clip))
-      ))
+        (clip/pause clip))))
 
   (set-volume [_this volume-percent]
     (swap! state assoc :volume volume-percent)

commit 4cc9865c3ac001c0cd11f74b8ad0541a666db154

Fix seek and device swapping caused by d8fa7a6
Also skips playing after a (seek) if closed

src/core/gakki/player/track/core.cljs
@@ -25,6 +25,15 @@
       (clip/play))
     (assoc current-state :clip clip)))
 
+(defn- swap-close! [state & {:keys [closed?]
+                             :or {closed? true}}]
+  (swap! state (fn [{:keys [clip] :as state}]
+                 (when clip
+                   (clip/close clip))
+                 (-> state
+                     (dissoc :clip)
+                     (assoc :closed? closed?)))))
+
 (defprotocol IAudioTrack
   (id [this])
   (close [this])
@@ -43,13 +52,9 @@
   IAudioTrack
   (id [_this] id)
 
-  (close [_this]
-    (swap! state (fn [{:keys [clip] :as state}]
-                   (when clip
-                     (clip/close clip))
-                   (-> state
-                       (dissoc :clip)
-                       (assoc :closed? true)))))
+  (close [this]
+    (log this " .close")
+    (swap-close! state))
 
   (read-config [_this]
     (pcm/read-config source))
@@ -61,11 +66,12 @@
             was-playing? (clip/playing? this)]
       ; Simply close any existing clip, reconfigure our start-time,
       ; and start again:
-      (close this)
+      (swap-close! state :closed? false)
       (swap! state assoc
              :seek-time timestamp-seconds
              :seek-bytes bytes-to-skip)
-      (when was-playing?
+      (when (and was-playing?
+                 (not (:closed? @state)))
         (clip/play this))))
 
   IAudioClip
@@ -83,16 +89,18 @@
 
   (play [this]
     (let [{:keys [clip closed?]} @state]
-      (when-not closed?
+      (if-not closed?
         (cond
           (and clip (clip/default-output-device? clip))
-          (clip/play clip)
+          (do (log this " Still default device; resume")
+              (clip/play clip))
 
           ; We have a clip, but it's targeting the wrong device
           clip
           (let [seek-time (clip/current-time this)]
-            ; Close this clip, seek to where we were, and start again
-            (close this)
+            ; Close this clip (without marking this Track as closed),
+            ; seek to where we were, and start again
+            (swap-close! state :closed? false)
             (p/do!
               (seek this seek-time)
               (clip/play this)))
@@ -104,7 +112,8 @@
               (log this " Resolved stream and config when already closed")
               (do
                 (>evt [:player/on-playback-config-resolved id config])
-                (swap! state create-clip config stream))))))))
+                (swap! state create-clip config stream)))))
+        (log this " Received (play) while closed"))))
 
   (pause [this]
     (when-let [clip (:clip @state)]