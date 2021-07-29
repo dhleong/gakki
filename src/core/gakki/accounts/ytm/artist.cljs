ee991f2946cf7196ed71674b4af94ded22f82fa0
Daniel Leong <me@dhleong.net>
2021-07-29 09:35:34 -0400

    Fix naming of artist shuffle/radio playlists
    They don't seem to resolve, however....

--- src/core/gakki/accounts/ytm/artist.cljs
+++ src/core/gakki/accounts/ytm/artist.cljs
@@ -18,7 +18,7 @@
     {:id radio-id
      :kind :playlist
      :provider :ytm
-     :title (str title " Radio")}))
+     :title title}))
 
 (defn load [^YTMusic client id]
   (p/let [response (send-request (.-cookie client)
@@ -50,6 +50,6 @@
      :description (-> header
                       (j/get :description)
                       (runs->text))
-     :radio (unpack-playlist header :startRadioButton (str "Shuffle " title))
-     :shuffle (unpack-playlist header :playButton (str title " Radio"))
+     :radio (unpack-playlist header :startRadioButton (str title " Radio"))
+     :shuffle (unpack-playlist header :playButton (str "Shuffle " title))
      :categories (keep music-shelf->section raw-rows)}))
